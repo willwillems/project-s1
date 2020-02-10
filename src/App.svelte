7efa34858452d5186bffd7ff200bdf28b4e3e8ad
Rutger Willems <rut.willems@gmail.com>
2020-02-10 22:00:02 +0200

    change button titles to something less formal

--- src/App.svelte
+++ src/App.svelte
@@ -11,8 +11,8 @@
 	<article id="main">
 		<h1>Project S1</h1>
 		<div class="button-container">
-			<a class="button" href="mailto:s1@willwillems.com?subject=S1%20Dev%20Kit&amp;body=You%20could%20leave%20this%20empty%20or%20just%20say%20hi!">Reserve a Dev kit</a>
-			<a class="button" href="mailto:s1@willwillems.com?subject=S1%20Finished%20Unit&amp;body=You%20could%20leave%20this%20empty%20or%20just%20say%20hi!">Reserve a final unit</a>
+			<a class="button" href="mailto:s1@willwillems.com?subject=S1%20Dev%20Kit&amp;body=You%20could%20leave%20this%20empty%20or%20just%20say%20hi!">I want a dev kit</a>
+			<a class="button" href="mailto:s1@willwillems.com?subject=S1%20Finished%20Unit&amp;body=You%20could%20leave%20this%20empty%20or%20just%20say%20hi!">I want a finished unit</a>
 		</div>
 		{@html TheText}
 	</article>
