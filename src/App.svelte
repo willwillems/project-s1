eab000af56c5062e6049f79d19cf46bcd45d337b
Rutger Willems <rut.willems@gmail.com>
2020-05-19 22:29:12 +0200

    add new logo to page

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
 <TheModel></TheModel>
 <main>
 	<article id="main">
-		<h1>Project S1</h1>
+		<h1> <img src="/logo.svg" alt="project logo" id="logo"/> Project S1</h1>
 		<div class="button-container">
 			<a class="button" href="mailto:s1@willwillems.com?subject=S1%20Dev%20Kit&amp;body=You%20could%20leave%20this%20empty%20or%20just%20say%20hi!">I want a dev kit</a>
 			<a class="button" href="mailto:s1@willwillems.com?subject=S1%20Finished%20Unit&amp;body=You%20could%20leave%20this%20empty%20or%20just%20say%20hi!">I want a finished unit</a>
